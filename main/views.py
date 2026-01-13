02ef9cd1bdfe2de4c4c3fb07c47bc9377b216d49
Marsh <holyfield444@gmail.com>
2026-01-13 15:14:23 +0100

    removed the contact view

--- main/views.py
+++ main/views.py
@@ -12,21 +12,3 @@ def about(request):
 def projects(request):
     return render(request, "main/projects.html")
 
-def contact(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            #still have to debug this soon
-            Contact.objects.create(
-                name=form.cleaned_data["name"],
-                email=form.cleaned_data["email"],
-                message=form.cleaned_data["message"]
-            )
-            return redirect("contact_success")
-    else:
-        form = ContactForm()
-
-    return render(request, "main/contact.html", {"form": form})
-
-def contact_success(request):
-    return render(request, "main/contact_success.html")
